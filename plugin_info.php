a21bc6541ccad3424d42f2af12296843785d2b93
mattpascoe <matt@opennetadmin.com>
2012-10-23 12:02:15 -0600

    fix plugin description

--- plugin_info.php
+++ plugin_info.php
@@ -4,7 +4,7 @@ Your plugin must have a short description and a version number
 */
 $plug_name = basename(dirname(__FILE__));
 
-$plugin_description = 'Track devices within datacenter racks';
+$plugin_description = 'Detailed host information output for programs';
 $plugin_version = '1.0';
 $plugin_help_url = 'http://opennetadmin.com/docs/plugins/'.$plug_name;
 
